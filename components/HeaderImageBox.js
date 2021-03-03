d26b4a0cc7d12d49126f94536b824e19b114fc3a
Ken Tauchi <kentauchica@gmail.com>
2021-03-03 01:59:33 -0800

    add scroll down arrow icon animation

--- components/HeaderImageBox.js
+++ components/HeaderImageBox.js
@@ -1,14 +1,19 @@
 import React from "react";
 import styled from "styled-components";
 import Button from "./ReusableElements/Button";
-// import banner from "../assets/img/home-banner-mobile.jpg";
+import ScrollArrow from "./ReusableElements/ScrollArrow";
 
 const HeaderImageBox = ({ type, title, desc, btnText, img }) => {
+  const scrollDown = () => {
+    let pageHeight = window.innerHeight;
+    window.scrollBy(0, pageHeight);
+  };
   return (
     <HeaderContainer img={img}>
       <h1>{title}</h1>
-      <p>{desc}</p>
+      <HeaderP>{desc}</HeaderP>
       <Button text={btnText} />
+      <ScrollArrow onClick={scrollDown} />
     </HeaderContainer>
   );
 };
@@ -23,6 +28,21 @@ const HeaderContainer = styled.div`
   background-position: center;
   background-size: cover;
   height: 100vh;
+  position: relative;
+  padding: 1rem;
+  &::after {
+      content: "";
+      position: absolute;
+      top: 0;
+      left: 0;
+      height: 100vh;
+      width: 100%;
+      background: rgba(241, 90, 34, 0.3);
+      display: block;
+`;
+
+const HeaderP = styled.p`
+  margin: 3rem 0;
 `;
 
 export default HeaderImageBox;
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,8 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import "../components/ReusableElements/ScrollArrow.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
