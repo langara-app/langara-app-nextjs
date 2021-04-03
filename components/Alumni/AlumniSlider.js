f5e7f080053d8bdfc7093952a94fdb2930257d91
Ken Tauchi <kentauchica@gmail.com>
2021-04-03 16:48:56 -0700

    fix image alignment, style mobile menu, fixed route path

--- components/Alumni/AlumniSlider.js
+++ components/Alumni/AlumniSlider.js
@@ -35,9 +35,36 @@ const AlumniSliderContainer = styled.div`
   backgroundcolor: white;
   margin: 0 2rem;
 
+  .slick-list {
+    height: 91vw !important;
+    display: flex !important;
+    flex-direction: column !important;
+    justify-content: center !important;
+  }
+
+  .slick-dots li {
+    margin: 0 !important;
+  }
+  .slick-dots li button:before {
+    font-size: 11px !important;
+  }
+  .slick-dots li.slick-active button:before {
+    color: white !important;
+  }
+
   @media only screen and (min-width: 768px) {
     width: 50vw !important;
     margin: 0;
+    .slick-list {
+      height: 50vw !important;
+    }
+
+    .slick-dots li button:before {
+      font-size: 13px !important;
+    }
+    .slick-dots li.slick-active button:before {
+      color: #675d51 !important;
+    }
   }
 `;
 
