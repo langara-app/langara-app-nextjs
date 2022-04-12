f89d24db11c6c21688fb338690ae3ca16bc717b2
Jaspreet Singh Bhatti <ijaspreetbhatti@outlook.com>
2022-04-11 17:22:57 -0700

    Cleanup AboutUs

--- pages/program-overview/index.js
+++ pages/program-overview/index.js
@@ -1,38 +1,13 @@
 import React from "react";
 import Head from "next/head";
 import { WmddData } from '../../lib/WmddData';
-import WmddIntro from '../../components/WMDD/WmddIntro';
-import styles from '../../styles/WMDD.module.css';
-import AdminBox from '../../components/AdminBox';
-import DevSlider from '../../components/WMDD/DevSlider';
-import DesSlider from '../../components/WMDD/DesSlider';
-import ataglance from '../../assets/ataglance.svg';
-import AfterProgram from '../../components/WMDD/AfterProgram';
-import ProgramCurriculum from '../../components/WMDD/ProgramCurriculum';
 
 const AboutUs = () => {
   return (
     <>
       <Head>
-        <title>Project Overview</title>
+        <title>{WmddData.header.title}</title>
       </Head>
-      <div className={styles.wmdd_body}>
-        <div className={styles.wmdd_full_intro}>
-          <div className={styles.wmdd_intro}>
-            <WmddIntro
-              subtitle={WmddData.header.title}
-              title={WmddData.header.subtitle}
-              desc={WmddData.header.description}
-            />
-          </div>
-          <img src={ataglance} className={styles.wmdd_image} />
-        </div>
-        <ProgramCurriculum />
-        <DevSlider />
-        <DesSlider />
-        <AfterProgram />
-        <AdminBox />
-      </div>
     </>
   );
 };
