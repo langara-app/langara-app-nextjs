0530c73a4f3f40d466269c93e82d150f8232014f
Jaspreet Singh Bhatti <ijaspreetbhatti@outlook.com>
2022-04-15 12:54:12 -0700

    Fixes Card Width for ProjectIntro Mobile View

--- components/Project/ProjectIntro.js
+++ components/Project/ProjectIntro.js
@@ -25,22 +25,23 @@ const ProjectIntro = (props) => {
 };
 
 const Container = styled.div`
-.imgWrap{
-  width: 356px;
-  height: 236px;
+  overflow: hidden;
+  .imgWrap{
+    max-width: 358px;
+    height: 236px;
 
-  img{
-    width: 100%;
-    height: 100%;
-    object-fit: cover;
-    overflow: hidden;
-    border-top-left-radius: 4px;
-    border-top-right-radius: 4px;
+    img{
+      width: 100%;
+      height: 100%;
+      object-fit: cover;
+      overflow: hidden;
+      border-top-left-radius: 4px;
+      border-top-right-radius: 4px;
+    }
   }
-}
 
   .card{
-    width: 358px;
+    max-width: 358px;
     height: 399px;
     border-radius: 4px;
     border: 1px solid #B0BEC5;
