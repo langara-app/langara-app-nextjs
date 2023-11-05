a3cbbdf3652c74257f151f0adfedff8e21b5411c
Karan Singh Dhir <ksinghdhir1@gmail.com>
2023-11-04 19:09:12 -0700

    fix: style imports

--- next.config.js
+++ next.config.js
@@ -7,6 +7,7 @@ const nextConfig = withImages({
     disableStaticImages: true,
   },
   compiler: {
+    // https://stackoverflow.com/questions/51791163/warning-prop-classname-did-not-match-when-using-styled-components-with-seman
     // Enables the styled-components SWC transform
     styledComponents: true
   },
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,33 @@
-import '@/styles/globals.css'
+// import '@/styles/globals.css'
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+// export default function App({ Component, pageProps }) {
+//   return <Component {...pageProps} />
+// }
+
+import "./_app.css";
+import "../styles/globals.css";
+import "swiper/scss";
+import "swiper/scss/navigation";
+import "swiper/scss/pagination";
+import "swiper/scss/scrollbar";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+import Head from "next/head";
+import MenuBar from "../components/MenuBar";
+import Footer from "../components/Footer";
+
+
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Head></Head>
+      <MenuBar />
+      {/* <Component {...pageProps} />
+      <Footer /> */}
+    </>
+  );
 }
+
+export default MyApp;
