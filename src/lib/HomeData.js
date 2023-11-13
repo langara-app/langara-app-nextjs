c9deff4864fe0ebd0858adf85e465e15ddf449eb
dacosio <dacosio@gmail.com>
2023-11-13 13:20:42 -0800

    removed period

--- src/lib/HomeData.js
+++ src/lib/HomeData.js
@@ -12,7 +12,7 @@ export const HomeData = {
   },
   header: {
     title: "Web and Mobile App Design and Development Program",
-    description1: "Your career in the technology industry starts here.",
+    description1: "Your career in the technology industry starts here",
     description2:
       "Learn how to design and develop real-world web and mobile applications - from start to finish!",
   },
