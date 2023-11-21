9ea67706988342186ff837e5573fb3cec24c53b7
Karan Singh Dhir <ksinghdhir1@gmail.com>
2023-11-21 15:25:18 -0800

    fix: date parsing bug

--- src/components/Project/NewsCard.js
+++ src/components/Project/NewsCard.js
@@ -10,11 +10,9 @@ import EventCardButton from "./EventCardButton.js";
 // Split and convert into Wed, Dec 7, 2021 format
 function formatDate(dateString) {
   var dateComponents = dateString.split("/");
-  var dateObject = new Date(dateComponents[2], dateComponents[0] - 1, dateComponents[1]);
-
+  var dateObject = new Date(`${dateComponents[1]+"/"+dateComponents[0]+"/"+dateComponents[2]}`);
   var daysOfWeek = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
   var months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
-
   var formattedDate = daysOfWeek[dateObject.getDay()] + ', ' + months[dateObject.getMonth()] + ' ' + dateObject.getDate() + ', ' + dateObject.getFullYear();
   return formattedDate
 }
