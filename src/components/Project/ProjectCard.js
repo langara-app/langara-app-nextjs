63171311e532aaf7b08b378c48b0d7c6b57c796d
Karan Singh Dhir <ksinghdhir1@gmail.com>
2023-11-21 16:42:14 -0800

    optimise: image loading

--- src/components/Project/ProjectCard.js
+++ src/components/Project/ProjectCard.js
@@ -1,4 +1,3 @@
-
 import React, { useState } from "react";
 import styled from "styled-components";
 import Link from "next/link";
@@ -6,16 +5,21 @@ import Image from "next/image";
 import useWindowWidth from "../Hooks/useWindowWidth";
 import { CommonStyling } from "../../lib/CommonStyling";
 
-
-
 const ProjectCard = ({ cardData, showOutline }) => {
-  const {slug, picture, name, description } = cardData;
+  const { slug, picture, name, description } = cardData;
   return (
     <Container data-cardborder={showOutline}>
       <div className="card">
         <Link href={`/projects/${slug}`}>
           <div className="imgWrap">
-            <img src={picture} alt="project image" />
+            <Image
+              loading="lazy"
+              unoptimized={true}
+              src={picture}
+              width={260}
+              height={260}
+              alt={`${name} app cover`}
+            />
           </div>
           <div className="imgTextWrap">
             <h3 className="projectTitle">{name}</h3>
@@ -28,46 +32,45 @@ const ProjectCard = ({ cardData, showOutline }) => {
 };
 
 const Container = styled.div`
+  .card {
+    width: 260px;
+    height: 450px;
+    border-radius: 1rem;
+    background-color: ${CommonStyling.backgroundColor};
+    border: ${(props) =>
+      props["data-cardborder"] ? "2px solid #E6E6E6" : "none"};
+  }
+  .imgTextWrap {
+    padding: 1rem;
+  }
 
-.card {
-  width: 260px;
-  height: 450px;
-  border-radius: 1rem;
-  background-color: ${CommonStyling.backgroundColor};
-  border: ${props => (props['data-cardborder'] ? '2px solid #E6E6E6' : 'none')};
+  .imgTextWrap > .projectTitle {
+    color: ${CommonStyling.primaryColor};
+    font-size: ${CommonStyling.h3FontSize};
+    font-weight: 500;
+    margin-bottom: 0.2rem;
+  }
 
-}
-.imgTextWrap {
-  padding: 1rem;
-}
+  p.projectDesc {
+    font-size: ${CommonStyling.body2FontSize};
+    display: -webkit-box;
+    -webkit-box-orient: vertical;
+    overflow: hidden;
+    -webkit-line-clamp: 5; /* Set the number of lines to display */
+  }
 
-.imgTextWrap > .projectTitle {
-  color: ${CommonStyling.primaryColor};
-  font-size: ${CommonStyling.h3FontSize};
-  font-weight: 500;
-  margin-bottom: .2rem;
-}
+  .imgWrap {
+    overflow: hidden;
+    border-top-left-radius: 1rem;
+    border-top-right-radius: 1rem;
+  }
 
-p.projectDesc {
-  font-size: ${CommonStyling.body2FontSize};
-  display: -webkit-box;
-  -webkit-box-orient: vertical;
-  overflow: hidden;
-  -webkit-line-clamp: 5; /* Set the number of lines to display */
-}
-
-.imgWrap {
-  overflow: hidden;
-  border-top-left-radius: 1rem;
-  border-top-right-radius: 1rem;
-}
-
-.imgWrap img {
-  display: block;
-  aspect-ratio: 1/1;
-  object-fit: cover;
-  transform: scale(1.5);
-}
+  .imgWrap img {
+    display: block;
+    aspect-ratio: 1/1;
+    object-fit: cover;
+    transform: scale(1.5);
+  }
 `;
 
 export default ProjectCard;
