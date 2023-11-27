48984b8da0dbaa0bd3f516656845943c724af3c5
Don Cosio <dacosio@gmail.com>
2023-11-26 16:21:33 -0800

    initial homepahe responsveness

--- src/components/AdminBox.js
+++ src/components/AdminBox.js
@@ -5,6 +5,7 @@ import useWindowWidth from "./Hooks/useWindowWidth";
 import btmImage from "../assets/img/wmdd/homeBottom.png";
 import btmImage1920 from "../assets/img/wmdd/homeBtm1920.png";
 import btmImage1440 from "../assets/img/wmdd/homeBtm1440.png";
+import btmImage1366 from "../assets/img/wmdd/homeBtm1366.png";
 
 import { HomeData } from "../lib/HomeData";
 import { CommonStyling } from "../lib/CommonStyling";
@@ -17,9 +18,9 @@ const AdminBox = () => {
       return btmImage1920;
     } else if (width < 1920 && width >= 1440) {
       return btmImage1440;
-    } else if (width < 1440 && width >= 1024) {
-      return btmImage;
-    } else if (width < 1024 && width >= 768) {
+    } else if (width < 1440 && width >= 1360) {
+      return btmImage1366;
+    } else if (width < 1360 && width >= 768) {
       return btmImage;
     } else if (width < 768 && width >= 695) {
       return btmImage1440;
@@ -89,11 +90,14 @@ const BtmImg = styled.img`
     height: 100%;
   }
 
-  @media screen and (max-width: 1439px) and (min-width: 1155px) {
-    object-position: right;
+  @media screen and (max-width: 1439px) and (min-width: 1361px) {
+    object-position: center;
   }
-  @media screen and (max-width: 1154px) and (min-width: 870px) {
-    object-position: 90%;
+  @media screen and (max-width: 1360px) and (min-width: 1361px) {
+    object-position: center;
+  }
+  @media screen and (max-width: 1359px) and (min-width: 870px) {
+    object-position: right;
   }
 
   @media screen and (max-width: 869px) and (min-width: 768px) {
