d05d2bd0d9c108d32908d57448458faf596c34ce
Karan Singh Dhir <ksinghdhir1@gmail.com>
2024-07-24 14:01:51 -0700

    fix: footer layout fixes on all screens

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,58 +1,15 @@
 import React from "react";
 import Link from "next/link";
 import styled from "styled-components";
-import useWindowWidth from "./Hooks/useWindowWidth";
 import logo2 from "../assets/logo2.svg";
 
-import { HomeData } from "../lib/HomeData";
 import { MenuData } from "../lib/MenuData";
 import { CommonStyling } from "../lib/CommonStyling";
 import Image from "next/image";
 import Button from "../components/ReusableElements/Button";
 
 const Footer = () => {
-  const width = useWindowWidth();
-
-  return width < 850 ? (
-    <Container>
-      <LinkWrapper>
-        <Logo>
-          <Image src={logo2} width={87.9} height={41.65} alt="Logo" />
-        </Logo>
-        {MenuData.map((menu, index) => {
-          return (
-            <Link
-              href={menu.link}
-              key={index}
-              className="menu-item"
-              style={{ paddingLeft: "10px" }}
-            >
-              {menu.title}
-            </Link>
-          );
-        })}
-        <Button
-          link={"https://langara.ca/admissions/apply-to-langara/index.html"}
-          text="Apply"
-          color="#FFFFFF"
-          bcg="#F15A22"
-          font={CommonStyling.body2FontSize.split("r")[0]}
-        />
-      </LinkWrapper>
-      <CopyRight>
-        <span>
-          &copy; {new Date().getFullYear()} Langara College. All rights
-          reserved.
-        </span>
-        <a
-          href="https://langara.ca/about-langara/records-management-and-privacy/privacy/privacy-statement.html"
-          target="_blank"
-        >
-          Privacy Policy
-        </a>
-      </CopyRight>
-    </Container>
-  ) : (
+  return (
     <FlexContainer>
       <LinkWrapper>
         <Logo>
@@ -92,22 +49,14 @@ const Footer = () => {
   );
 };
 
-const Container = styled.div`
-  background-color: #ffffff;
-  display: flex;
-  flex-direction: column;
-  align-items: flex-start;
-  color: #21263a;
-  padding: 16px 48px;
-`;
-
 const FlexContainer = styled.div`
   background-color: #ffffff;
   display: flex;
   flex-direction: column;
+  padding: 1rem 3rem;
 
-  @media screen and (min-width: 768px) {
-    padding: 1rem 3rem;
+  @media screen and (max-width: 850px) {
+    align-items: flex-start;
   }
 `;
 
@@ -115,7 +64,7 @@ const LinkWrapper = styled.div`
   display: flex;
   flex-direction: column;
   align-items: flex-start;
-  gap: 4rem;
+  gap: 2rem;
   padding: 1rem 0;
   .menu-items-wrapper {
     display: flex;
@@ -130,7 +79,14 @@ const LinkWrapper = styled.div`
     }
   }
 
-  @media only screen and (min-width: 768px) {
+  @media only screen and (max-width: 850px) {
+    .menu-items-wrapper {
+      flex-direction: column;
+      align-items: flex-start;
+    }
+  }
+
+  @media only screen and (min-width: 850px) {
     flex-direction: row;
     justify-content: space-between;
     align-items: center;
@@ -165,11 +121,11 @@ const CopyRight = styled.div`
   align-items: center;
   gap: 1rem;
   padding-top: 0;
-  padding-bottom: 8px;
-  @media only screen and (min-width: 768px) {
+  padding-bottom: 0.5rem;
+  padding-top: 1rem;
+  @media only screen and (min-width: 850px) {
     flex-direction: row;
     justify-content: space-between;
-    padding-top: 10px;
   }
 `;
 
